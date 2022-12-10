624f1af27e2b56a25f4f5e8bc90bf288605a8a8f
Nikhil <nikhilk21234@gmail.com>
2022-12-10 16:58:00 +0530

    Reducing Delay More

--- app/src/main/java/com/example/reflex_traing_device_3_0/ReflexTraining.java
+++ app/src/main/java/com/example/reflex_traing_device_3_0/ReflexTraining.java
@@ -252,7 +252,7 @@ public class ReflexTraining extends AppCompatActivity implements View.OnClickLis
             */
 
             try {
-                Thread.sleep(200);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
